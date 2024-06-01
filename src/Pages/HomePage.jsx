4e5ccc9671efe0fe6c81562bc87f23068a422417
yungtoby <tooobbbi.urmaker@gmail.com>
2024-06-01 15:48:17 +1000

    homepage.jsx/css update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,40 @@
+import { Link } from 'react-router-dom';
+
+import main_header from '../Images/main_header.png'
 import './HomePage.css'
 
 function HomePage() {
   return (
     <>
       <div className="homePage_wrapper">
-        <h1>Your homepage!</h1>
+        <div className="homePageHeader">
+            <img src={main_header} />
+        </div>
+
+        <div className="summaryContent">
+            <h2>Summary</h2>
+        </div>
+
+        <div className="routingContent">
+            <RouteButton ButtonName={"Button 1"} LinkTo={"/"} />
+            <RouteButton ButtonName={"Button 2"} LinkTo={"/"}  />
+            <RouteButton ButtonName={"Button 3"} LinkTo={"/"}  />
+            <button id="signoutButton">Sign Out</button>
+        </div>
+
+        <div>
+
+        </div>
       </div>
     </>
   )
+}
+
+// React function to create and return a link button
+function RouteButton({ ButtonName, LinkTo }) {
+    return (
+    <Link to={LinkTo}><button>{ButtonName}</button></Link>
+    );
 }
 
 export default HomePage;
